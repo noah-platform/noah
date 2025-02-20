54e65543b58f663ea0b0d202156a62b40339e352
Thanayut T <b@thanayut.in.th>
2025-02-21 00:55:07 +0700

    feat(frontend): enable sentry

--- frontend/instrument.server.mjs
+++ frontend/instrument.server.mjs
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
-  enabled: false,
+  enabled: true,
 
   dsn: 'https://cd257a261d3aa8b073ce3c2f7fb35462@o1418462.ingest.us.sentry.io/4508830487543808',
   integrations: [nodeProfilingIntegration()],
