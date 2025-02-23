cffe05069153fa4426df60877fe6588998027fe9
Thanayut T <b@thanayut.in.th>
2025-02-24 00:08:28 +0700

    refactor(frontend): home

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,24 +1,25 @@
 import type { Route } from './+types/index';
-import logo from './logo.svg';
-import slide_1 from './slide_1.svg';
-import slide_2 from './slide_2.svg';
-import slide_3 from './slide_3.svg';
-import slide_4 from './slide_4.svg';
-import std_1 from './std_1.svg';
-import std_2 from './std_2.svg';
-import std_3 from './std_3.svg';
-import teacher_1 from './teacher_1.svg';
-import teacher_2 from './teacher_2.svg';
-import teacher_3 from './teacher_3.svg';
-import listening from './listening.svg';
-import writing from './writing.svg';
-import reading from './reading.svg';
-import speaking from './speaking.svg';
-import click from './click.svg';
-import dashboard from './dashboard.svg';
+import logo from '~/assets/home/logo.svg';
+import slide_1 from '~/assets/home/slide_1.svg';
+import slide_2 from '~/assets/home/slide_2.svg';
+import slide_3 from '~/assets/home/slide_3.svg';
+import slide_4 from '~/assets/home/slide_4.svg';
+import std_1 from '~/assets/home/std_1.svg';
+import std_2 from '~/assets/home/std_2.svg';
+import std_3 from '~/assets/home/std_3.svg';
+import teacher_1 from '~/assets/home/teacher_1.svg';
+import teacher_2 from '~/assets/home/teacher_2.svg';
+import teacher_3 from '~/assets/home/teacher_3.svg';
+import listening from '~/assets/home/listening.svg';
+import writing from '~/assets/home/writing.svg';
+import reading from '~/assets/home/reading.svg';
+import speaking from '~/assets/home/speaking.svg';
+import click from '~/assets/home/click.svg';
+import dashboard from '~/assets/home/dashboard.svg';
 import { FaUser } from 'react-icons/fa';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Noah English' }];
@@ -42,7 +43,7 @@ function NavBar() {
     <nav className="bg-[#070559] text-white px-6 py-4 flex justify-between items-center">
       {/* Logo */}
       <div className="flex items-center">
-        <img src={logo} alt="NOAH ENGLISH" className="w-18 h-12 mr-4" />
+        <img src={logo} alt="Noah English" className="w-18 h-12 mr-4" />
         <span className="text-xl font-bold">NOAH ENGLISH</span>
       </div>
 
@@ -56,9 +57,11 @@ function NavBar() {
 
       {/* Authentication Buttons */}
       <div className="flex items-center space-x-4">
-        <button className="bg-[#EF1B31] font-semibold text-white px-4 py-2 rounded-full cursor-pointer">
-          Sign up | Log in
-        </button>
+        <Link to="/login">
+          <button className="bg-[#EF1B31] font-semibold text-white px-4 py-2 rounded-full cursor-pointer">
+            Sign up | Log in
+          </button>
+        </Link>
         <FaUser className="text-white text-4xl cursor-pointer bg-[#D9D9D9] rounded-full p-2" />
       </div>
     </nav>
@@ -248,7 +251,7 @@ function ReviewPage() {
       {
         img: teacher_1,
         title: 'IELTS Senior Grader Review',
-        desc: 'NOAH English aligns well with the official IELTS format, offering diverse topics and challenges. While the Writing and Speaking modules are solid, more detailed feedback on essay structure and fluency, along with model answers, would enhance its value for teaching.',
+        desc: 'Noah English aligns well with the official IELTS format, offering diverse topics and challenges. While the Writing and Speaking modules are solid, more detailed feedback on essay structure and fluency, along with model answers, would enhance its value for teaching.',
       },
       {
         img: teacher_2,
@@ -258,7 +261,7 @@ function ReviewPage() {
       {
         img: teacher_3,
         title: 'IELTS Teacher Review',
-        desc: "NOAH English does well in simulating test conditions, especially in Reading and Listening. However, Speaking and Writing feedback needs refinement to better match real-world grading. It's a strong tool with room for targeted enhancements.",
+        desc: "Noah English does well in simulating test conditions, especially in Reading and Listening. However, Speaking and Writing feedback needs refinement to better match real-world grading. It's a strong tool with room for targeted enhancements.",
       },
     ],
   ];
